2565fa706777d0f496b290d0981f17251eb7e9a7
Benching <jordanlacandula@gmail.com>
2019-06-01 00:51:22 +0800

    Initial Setup with vue installed

---
+++ init.bat
@@ -0,0 +1,15 @@
+composer require sec-checker --dev
+composer install
+composer require twig
+composer require profiler --dev
+composer require debug --dev
+composer require asset
+composer require annotation
+composer require encore
+composer require maker
+npm install webpack-notifier --only=dev
+npm install bulma
+npm install copy-webpack-plugin --only=dev
+npm install sass-loader node-sass --only=dev
+npm install vue vue-loader@^15.0.11 vue-template-compiler --save-dev
+npm install
