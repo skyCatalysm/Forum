b5b5aa8587a038d766c174e016c3d38421f1e046
Benching <jordanlacandula@gmail.com>
2019-06-11 14:32:00 +0800

    reset migration

--- src/Migrations/Version20190611063050.php
+++ src/Migrations/Version20190611063050.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190603114400 extends AbstractMigration
+final class Version20190611063050 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,7 @@ final class Version20190603114400 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE accounts');
-        $this->addSql('ALTER TABLE user ADD email VARCHAR(255) NOT NULL, ADD password VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user DROP username');
     }
 
     public function down(Schema $schema) : void
@@ -31,7 +30,6 @@ final class Version20190603114400 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE accounts (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, password VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE user DROP email, DROP password');
+        $this->addSql('ALTER TABLE user ADD username VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
